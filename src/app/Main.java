1add3c3db398066c6ef6cc599827ca27be34d0a2
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-08-16 20:17:03 +0300

    add new fucntion

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Friend");
+        System.out.println(add(3, 5));
+    }
+    private static int add(int a, int b){
+        return a + b;
     }
 }
