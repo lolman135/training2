e9daaf1234869ea7ee40f3be13eae766ffa3ff6f
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-08-16 20:25:55 +0300

    change first message

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,7 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("This is new World");
         System.out.println("Friend");
         System.out.println(add(3, 5));
     }
